0b4b7667eb2321ed7daf6cccbeda27a1253ca786
BraydonKains <kainsbraydon@gmail.com>
2018-08-22 14:23:46 -0400

    ALLEGRO 5 NOW BITCHES

--- PocketTrivia/PocketTrivia/main.cpp
+++ PocketTrivia/PocketTrivia/main.cpp
@@ -1,19 +1,15 @@
-#include <allegro.h>
-#include <cstdint>
-#include <stdint.h>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_font.h>
 
-using namespace std;
-
-int main() {
-	allegro_init();
-	set_gfx_mode(GFX_SAFE, 640, 480, 0, 0);
-	install_keyboard();
-
-	textout_ex(screen, font, "fuck athabasca", 1, 1, 10, -1);
-	textout_ex(screen, font, "fuck athabasca escape to quit", 1, 1, 10, -1);
-	while (!key[KEY_ESC]);
-	allegro_exit();
+int main()
+{
+	al_init();
+	al_init_font_addon();
+	ALLEGRO_DISPLAY* display = al_create_display(800, 600);
+	ALLEGRO_FONT* font = al_create_builtin_font();
+	al_clear_to_color(al_map_rgb(0, 0, 0));
+	al_draw_text(font, al_map_rgb(255, 255, 255), 400, 300, ALLEGRO_ALIGN_CENTER, "Welcome to Allegro!");
+	al_flip_display();
+	al_rest(5.0);
 	return 0;
 }
-
-END_OF_MAIN()
